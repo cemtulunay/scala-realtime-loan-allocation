70796c23d37c8d40abe5ea840dc2ec7556099654
cemtulunay <ahmetcemtulunay@gmail.com>
2025-03-29 02:50:46 +0300

    create project

--- src/main/scala/playground/Playground1.scala
+++ src/main/scala/playground/Playground1.scala
@@ -1,10 +1,12 @@
 package playground
 
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink._
 
 object Playground1 {
 
   def main(args: Array[String]): Unit = {
+
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     val data = env.fromElements(1 to 1000: _*)
     data.print()
